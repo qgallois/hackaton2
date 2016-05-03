3aadee8d88e473019e937eb95714daa84437cb98
Paul Crayssac <paul-ka@laposte.net>
2016-05-04 00:32:29 +0200

    Add map

--- DefisIsLife/www/js/app.js
+++ DefisIsLife/www/js/app.js
@@ -6,7 +6,6 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
-
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -79,18 +78,10 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
     }
   });
 
-/*.config(function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-  .state('map', {
-    url: '/',
-    templateUrl: 'templates/tab-map.html',
-    controller: 'mapController'
-  });
 
   $urlRouterProvider.otherwise("/");
 
-})*/
 
 
   // if none of the above states are matched, use this as the fallback
--- DefisIsLife/www/js/services.js
+++ DefisIsLife/www/js/services.js
@@ -8,27 +8,37 @@ angular.module('starter.services', [])
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
-    face: 'img/ben.png'
+    face: 'img/ben.png',
+    lat: 48.558853,
+    lng: 1.045923
   }, {
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    face: 'img/max.png',
+    lat: 48.502766,
+    lng: 1.058319
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    face: 'img/adam.jpg',
+    lat: 48.503790,
+    lng: 1.019776
   }, {
     id: 3,
     name: 'Perry Governor',
     lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    face: 'img/perry.png',
+    lat: 48.520052,
+    lng: 0.979859
   }, {
     id: 4,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    face: 'img/mike.png',
+    lat: 48.536511,
+    lng: 1.154971
   }];
 
   return {
