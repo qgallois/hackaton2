dc2e9e62134b43e4b36427cb43c111e68c29ef51
Paul Crayssac <paul-ka@laposte.net>
2016-05-04 05:40:10 +0200

    Custumize list of users

--- DefisIsLife/www/js/services.js
+++ DefisIsLife/www/js/services.js
@@ -6,39 +6,44 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png',
+    name: 'Alex',
+    lastText: 'Tirer juste avec les nerfs',
+    face: 'img/Alex.jpg',
     lat: 48.558853,
-    lng: 1.045923
+    lng: 1.045923,
+    score:10
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png',
+    name: 'Emilie',
+    lastText: 'Supporter Thomas',
+    face: 'img/emilie.jpg',
     lat: 48.502766,
-    lng: 1.058319
+    lng: 1.058319,
+    score:6
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg',
+    name: 'Hadrien',
+    lastText: 'rester sur la même chaise 1h',
+    face: 'img/hadrien.jpg',
     lat: 48.503790,
-    lng: 1.019776
+    lng: 1.019776,
+    score:7
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png',
+    name: 'Jean Baptiste',
+    lastText: 'Faire 10 katas en 5 minutes',
+    face: 'img/jb.jpg',
     lat: 48.520052,
-    lng: 0.979859
+    lng: 0.979859,
+    score:10
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png',
+    name: 'Thomas',
+    lastText: 'Ne pas dire j\'ai faim d\'une journée',
+    face: 'img/thomas.jpg',
     lat: 48.536511,
-    lng: 1.154971
+    lng: 1.154971,
+    score:3
   }];
 
   return {
